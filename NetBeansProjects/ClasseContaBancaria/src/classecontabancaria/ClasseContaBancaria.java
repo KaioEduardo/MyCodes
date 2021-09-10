3064b4d3d80170142657537aa0103b0c5cacac94
KaioEduardo <90428052+KaioEduardo@users.noreply.github.com>
2021-09-09 21:19:59 -0300

    Add files via upload

---
+++ NetBeansProjects/ClasseContaBancaria/src/classecontabancaria/ClasseContaBancaria.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package classecontabancaria;
+
+/**
+ *
+ * @author aluno
+ */
+public class ClasseContaBancaria {
+
+    public static class Contabancaria {
+        
+        //Atributos
+        
+        public String nome;
+        public int agencia;
+        public int n_conta;
+        public float saldo;
+        
+        //Metodos
+        
+        public void saque(){
+            
+    
+        }
+        
+        public void trasnferencia(){
+            
+            
+        }
+        public static void main(String[]args){
+        
+            Contabancaria cont_nome = new Contabancaria();
+            Contabancaria cont_agencia = new Contabancaria();
+            Contabancaria cont_nconta = new Contabancaria();
+            Contabancaria cont_saldo  = new Contabancaria();
+        
+            
+            cont_nome.nome = George;
+            cont_agencia.agencia = 3924;
+            cont_nconta.n_conta  = 70240;
+            cont_saldo.saldo = 800,00;
+            
+            System.out.println
+        
+    }
+    
+    
+    
+}
---
+++ NetBeansProjects/ExemplosVariados/src/exemplosvariados/ExemplosVariados.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exemplosvariados;
+
+/**
+ *
+ * @author aluno
+ */
+public class ExemplosVariados {
+
+    public float soma;
+    public float salario;
+    
+    
+    
+    
+    
+    
+    public static int Soma(int n1,int n2)
+    
+    int soma;
+    soma = n1 + n2;
+    return soma;
+     
+    public static void ImprimeAumento(double salario,int percentual) {
+        
+        double aumento;
+        aumento = salario + salario * percentual / 100*0;
+        System.out.println("O salario com aumento é :" + aumento);
+        
+       
+    }
+    
+}
